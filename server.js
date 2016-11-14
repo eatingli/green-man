0844728f5a925885c6a2d0b95592d99e96dc336f
Eating-Lee <suwako.www@gmail.com>
2016-11-15 01:33:19 +0800

    Upload file to github.

---
+++ server.js
@@ -0,0 +1,27 @@
+var PORT = 8080;
+
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var router = express.Router();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+/*
+router.get('/test/:mode', function(req, res, next) {
+    var mode = req.params.mode;
+    console.log(mode);
+});
+*/
+
+router.post('/form', function(req, res, next) {
+    console.log(req.body);
+    res.send('success');
+});
+
+app.use('/api', router);
+
+app.listen(PORT, function() {
+    console.log('The server listening on port ' + PORT);
+});
